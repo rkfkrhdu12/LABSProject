52bf1436e405de3181903f5e0d65ab51bd14d81a
rkfkrhdu12 <33976974+rkfkrhdu12@users.noreply.github.com>
2019-06-15 11:18:28 +0900

    Add ScreenShake

---
+++ LABS/Assets/Scripts/2.InGame/ScreenShake.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenShake : MonoBehaviour
+{
+
+    // Transform of the camera to shake. Grabs the gameObject's transform
+    // if null.
+    public Transform camTransform;
+
+    // How long the object should shake for.
+    public float shake = 0f;
+
+    // Amplitude of the shake. A larger value shakes the camera harder.
+    public float shakeAmount = 0.1f;
+    public float decreaseFactor = 1.0f;
+
+    Vector3 originalPos;
+
+    void Awake()
+    {
+        if (camTransform == null)
+        {
+            camTransform = GetComponent(typeof(Transform)) as Transform;
+        }
+        OnEnable();
+    }
+
+    void OnEnable()
+    {
+        originalPos = camTransform.localPosition;
+    }
+
+    void Update()
+    {
+        if (shake > 0)
+        {
+            camTransform.localPosition = originalPos + Random.insideUnitSphere * shakeAmount;
+
+            shake -= Time.deltaTime * decreaseFactor;
+        }
+        else
+        {
+            shake = 0f;
+            camTransform.localPosition = originalPos;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Hit"))
+        {
+            shake = 1;
+        }
+    }
+
+}
